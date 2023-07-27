375c5ac18d61f7ad08866e40ba033d3ddeb4e1dc
fares@LAPTOP-9C455IB9 <faresibrahim@usf.edu>
2023-07-27 16:56:06 -0500

    aaa :wq
    ddddd
    :wq

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,17 +1,25 @@
-import {auth,provider} from "../config/firebase";
-import {signInWithPopup} from "firebase/auth";
-import {useNavigate} from "react-router-dom"
-export const Login=()=>{
-const navigate = useNavigate()
-   const signInwithGoogle= async ()=>{
-     const result = await signInWithPopup(auth,provider)
-     console.log(result);
-     navigate("/")
-     
-   }
+// Make sure "../config/firebase" file exports "auth" and "provider" correctly
+import { auth, provider } from "../config/firebase";
+import { signInWithPopup } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
 
-    return <div>
-        <p>Sign In with Google to continue</p>
-        <button onClick={signInwithGoogle}>Sign In with Google</button>
+export const Login = () => {
+  const navigate = useNavigate();
+
+  const signInWithGoogle = async () => {
+    try {
+      const result = await signInWithPopup(auth, provider);
+      console.log(result);
+      navigate("/");
+    } catch (error) {
+      console.error("Error signing in with Google:", error);
+    }
+  };
+
+  return (
+    <div>
+      <p>Sign In with Google to continue</p>
+      <button onClick={signInWithGoogle}>Sign In with Google</button>
     </div>
-}
+  );
+};
